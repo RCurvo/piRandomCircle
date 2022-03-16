54478729ae8c7bc817b1c2ee1a21678f6b020cd9
Renan Curvo <renan.curvo@gmail.com>
2022-03-16 12:48:08 -0300

    deleted useless code and indenting

--- sketch.js
+++ sketch.js
@@ -12,43 +12,42 @@ function setup() {
   strokeWeight(2);
 
 
-noFill();
+  noFill();
   ellipse(0, 0, r*2,r*2);
   rectMode(CENTER);
   rect(0,0,r*2,r*2);
-  
 }
 
 function draw() {
   translate(width/2, height/2);
 
   for (let i = 0; i < n+1; i++){
-const x = random(-r,r);
-const y = random(-r,r);
-total++;
+        const x = random(-r,r);
+        const y = random(-r,r);
+        total++;
 
-const d = x*x + y*y;
-if (d<r*r){
-  stroke (255,0,0)
-  circle++;
-}
-else {
-  stroke(255);
-}
+        const d = x*x + y*y;
+        if (d<r*r){
+          stroke (255,0,0)
+          circle++;
+        }
+        else {
+          stroke(255);
+        }
 
-point(x,y);
+        point(x,y);
   }
 
-const pi = 4*(circle/total);
-let c = color('black');
+  const pi = 4*(circle/total);
 
-noFill();
-stroke (0,0,0)
-piround = round(pi, 2);
-textSize(32);
-text(piround, -30, 10);
-if(n < 700){
-  n = n*1.005;
-}
+
+  noFill();
+  stroke (0,0,0)
+  piround = round(pi, 2);
+  textSize(32);
+  text(piround, -30, 10);
+  if(n < 700){
+    n = n*1.005;
+    }
 }
 
